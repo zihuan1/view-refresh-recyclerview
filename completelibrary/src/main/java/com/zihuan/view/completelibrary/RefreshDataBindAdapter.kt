5bfe0ff512985a39930ca07ab42fdd925a581976
紫幻 <31381762+zihuan1@users.noreply.github.com>
2020-09-03 10:29:44 +0800

    rename

--- completelibrary/src/main/java/com/zihuan/view/completelibrary/RefreshDataBindAdapter.kt
+++
@@ -1,7 +0,0 @@
-package com.zihuan.view.completelibrary
-
-import com.zihuan.baseadapter.RecyclerBindAdapter
-import com.zihuan.view.crvlibrary.RecyclerData
-
-class RefreshDataBindAdapter(`object`: Any?, res: Int) : RecyclerBindAdapter(`object`, res),
-    RecyclerData
