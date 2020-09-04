71a3bd22fc794b594553aacba95a450cc2ba5b1e
紫幻 <31381762+zihuan1@users.noreply.github.com>
2020-09-04 12:17:17 +0800

    t

--- app/src/main/java/com/zihuan/demo/Main3Activity.kt
+++ app/src/main/java/com/zihuan/demo/Main3Activity.kt
@@ -12,21 +12,30 @@ class Main3Activity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main3)
         var list = ArrayList<String>()
-
+        (0..10).forEach {
+            list.add("$it")
+        }
         re_view3.buildVerticalLayout(ReAdapter(this)).setData(list)
         var manager = re_view3.getBuilder()
+        manager.getRefreshLayout().isNestedScrollingEnabled = true
+        manager.setLoadingEnabled(true)
+        manager.getRecyclerView().isNestedScrollingEnabled = true
 //        扩展监听
         manager.setLoadListener(object : SimpleRefreshListener() {
             override fun onFusion(isRefresh: Boolean) {
                 Log.e("刷新/加载", "完成$isRefresh")
                 if (isRefresh) {
-                    (0..100).forEach {
+                    list.clear()
+                    (0..10).forEach {
                         list.add("$it")
                     }
                     manager.setData(list)
-//                    Log.e("刷新", "完成")
+                    Log.e("刷新", "完成"+list.size)
                 } else {
-                    list.clear()
+//                    list.clear()
+                    (0..10).forEach {
+                        list.add("$it")
+                    }
                     manager.setData(list)
                 }
             }
